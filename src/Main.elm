10401e89173f76fa87d6bf559b7a6b00154d2b54
Guy Dunton <guydunton@economist.com>
2019-11-20 09:00:02 +0000

    Add more cat gifs

--- src/Main.elm
+++ src/Main.elm
@@ -4,45 +4,82 @@ import Browser
 import Html exposing (Html, button, div, img, p, text)
 import Html.Attributes exposing (src)
 import Html.Events exposing (onClick)
+import Http
+import Json.Decode exposing (Decoder, at, string)
 
 
 type alias Model =
-    Int
+    { count : Int
+    , gifUrl : Maybe String
+    }
 
 
 type Msg
     = GoUp
     | GoDown
+    | GotGif (Result Http.Error String)
 
 
-init : Model
-init =
-    0
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { count = 0
+      , gifUrl = Nothing
+      }
+    , Http.get
+        { url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat"
+        , expect = Http.expectJson GotGif gifDecoder
+        }
+    )
+
+
+gifDecoder : Decoder String
+gifDecoder =
+    at [ "data", "image_url" ] string
 
 
 view : Model -> Html Msg
 view model =
     div []
         [ button [ onClick GoUp ] [ text "+" ]
-        , p [] [ text (String.fromInt model) ]
+        , p [] [ text (String.fromInt model.count) ]
         , button [ onClick GoDown ] [ text "-" ]
+        , displayGif model.gifUrl
         ]
 
 
-update : Msg -> Model -> Model
+displayGif : Maybe String -> Html Msg
+displayGif maybeUrl =
+    case maybeUrl of
+        Just url ->
+            p [] [ img [ src url ] [] ]
+
+        Nothing ->
+            p [] [ text "Image not loaded" ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GoUp ->
-            model + 1
+            ( { model | count = model.count + 1 }, Cmd.none )
 
         GoDown ->
-            model - 1
+            ( { model | count = model.count - 1 }, Cmd.none )
+
+        GotGif result ->
+            case result of
+                Ok url ->
+                    ( { model | gifUrl = Just url }, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
+    Browser.element
         { init = init
         , view = view
         , update = update
+        , subscriptions = \_ -> Sub.none
         }
